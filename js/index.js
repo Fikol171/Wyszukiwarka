9e83da7ae3c7073d0ca29524a6f2fa91a9b4cdbc
Fikol <pawelowiesek@gmail.com>
2023-03-10 16:27:37 +0100

    RegExp used

--- js/index.js
+++ js/index.js
@@ -3,9 +3,12 @@ const listItems = document.querySelectorAll(".js-listItem");
 
 const searchEngine = (event) => {
   const text = event.target.value.toLowerCase();
-
-  listItems.forEach((listItem) => {
-    listItem.textContent.toLowerCase().indexOf(text) !== -1
+   
+  listItems.forEach((listItem) => { 
+    
+    const match=new RegExp(search.value,"i").test(listItem.textContent)
+    match 
+    // listItem.textContent.toLowerCase().indexOf(text) !== -1
       ? (listItem.style.diplay = "block")
       : (listItem.style.display = "none");
   });
