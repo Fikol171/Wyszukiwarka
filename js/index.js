e7df817fd3777d3df3e44ff01ccf7d16e823d74e
Fikol <pawelowiesek@gmail.com>
2023-03-10 17:46:58 +0100

    closed code in a block,created new init function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
-const search = document.querySelector(".js-search");
-const listItems = document.querySelectorAll(".js-listItem");
+{
+  const search = document.querySelector(".js-search");
+  const listItems = document.querySelectorAll(".js-listItem");
 
-const searchEngine = (event) => {
-  const text = event.target.value.toLowerCase();
-   
-  listItems.forEach((listItem) => { 
-    
-    const match=new RegExp(search.value,"i").test(listItem.textContent)
-    match 
-    // listItem.textContent.toLowerCase().indexOf(text) !== -1
-      ? (listItem.style.diplay = "block")
-      : (listItem.style.display = "none");
-  });
-};
-search.addEventListener("keyup", searchEngine);
+  const onInputAction = () => {
+    listItems.forEach((listItem) => {
+      const match = new RegExp(search.value, "i").test(listItem.textContent);
+      match
+        ? (listItem.style.diplay = "block")
+        : (listItem.style.display = "none");
+    });
+  };
+  const searchEngine = () => {
+    onInputAction();
+  };
+
+  const init = () => {
+    search.addEventListener("keyup", searchEngine);
+    searchEngine();
+  };
+  init();
+}
