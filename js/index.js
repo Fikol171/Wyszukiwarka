0fccb962901fc3bec4f2b19abe4733117ce6ba97
Fikol <pawelowiesek@gmail.com>
2023-03-10 16:15:17 +0100

    changed if to ternary

--- js/index.js
+++ js/index.js
@@ -5,11 +5,9 @@ const searchEngine = (event) => {
   const text = event.target.value.toLowerCase();
 
   listItems.forEach((listItem) => {
-    if (listItem.textContent.toLowerCase().indexOf(text) !== -1) {
-      listItem.style.diplay = "block";
-    } else {
-      listItem.style.display = "none";
-    }
+    listItem.textContent.toLowerCase().indexOf(text) !== -1
+      ? (listItem.style.diplay = "block")
+      : (listItem.style.display = "none");
   });
 };
 search.addEventListener("keyup", searchEngine);
